ff72862b7e5702eaf7eadecff08b4468c25e8bbd
JackyAnn <JackyAnn@foxmail.com>
2022-06-17 14:38:23 +0800

    v1.0.1
    Signed-off-by: JackyAnn <JackyAnn@foxmail.com>

--- lib-arch-app-initializer/src/main/java/ltd/dolink/arch/initializer/ApplicationOwner.java
+++ lib-arch-app-initializer/src/main/java/ltd/dolink/arch/initializer/ApplicationOwner.java
@@ -2,20 +2,25 @@ package ltd.dolink.arch.initializer;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
+import androidx.lifecycle.HasDefaultViewModelProviderFactory;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ProcessLifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProvider.Factory;
 import androidx.lifecycle.ViewModelStore;
 import androidx.lifecycle.ViewModelStoreOwner;
 
-public class ApplicationOwner implements LifecycleOwner, ViewModelStoreOwner {
+public class ApplicationOwner implements LifecycleOwner, ViewModelStoreOwner, HasDefaultViewModelProviderFactory {
   private static ApplicationOwner applicationOwner;
   private final Application application;
-  private final ViewModelStore viewModelStore = new ViewModelStore();
-
+  private final ViewModelStore viewModelStore;
+  private final ViewModelProvider.Factory factory;
   public ApplicationOwner(Application application) {
     this.application = application;
     applicationOwner = this;
+    viewModelStore = new ViewModelStore();
+    factory = (Factory) ViewModelProvider.AndroidViewModelFactory.getInstance(application);
   }
 
   @NonNull
@@ -38,5 +43,11 @@ public class ApplicationOwner implements LifecycleOwner, ViewModelStoreOwner {
   @Override
   public Lifecycle getLifecycle() {
     return ProcessLifecycleOwner.get().getLifecycle();
+  }
+
+  @NonNull
+  @Override
+  public Factory getDefaultViewModelProviderFactory() {
+    return factory;
   }
 }
